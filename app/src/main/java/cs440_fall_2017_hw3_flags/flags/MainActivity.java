b979ece63b34f385a7a32924e2bf63d923ee6158
Ovie Umukoro <ovie.u.umukoro@gmail.com>
2017-09-30 16:21:18 -0400

    Add options menu to welcome page

--- app/src/main/java/cs440_fall_2017_hw3_flags/flags/MainActivity.java
+++ app/src/main/java/cs440_fall_2017_hw3_flags/flags/MainActivity.java
@@ -1,7 +1,9 @@
 package cs440_fall_2017_hw3_flags.flags;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+
+    /* create and inflate the menu */
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.continentmenu, menu);
+        return super.onCreateOptionsMenu(menu);
     }
 }
