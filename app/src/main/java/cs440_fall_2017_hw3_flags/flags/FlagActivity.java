3e48b57e83e0a8131e7d2b51303fe63135646a28
Ovie Umukoro <ovie.u.umukoro@gmail.com>
2017-10-04 16:22:10 -0400

    Add list of selected continent to game activity
    - save the continent drop down selection into
      the continent list
      side-effect: this remains active if the drop
      down is reselected, remeber to clear the
      continent selection list when the game ends.
    
    - send this saved continent list to the next
      activity through the use of intents.

---
+++ app/src/main/java/cs440_fall_2017_hw3_flags/flags/FlagActivity.java
@@ -0,0 +1,13 @@
+package cs440_fall_2017_hw3_flags.flags;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FlagActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_flag);
+    }
+}
--- app/src/main/java/cs440_fall_2017_hw3_flags/flags/MainActivity.java
+++ app/src/main/java/cs440_fall_2017_hw3_flags/flags/MainActivity.java
@@ -1,6 +1,8 @@
 package cs440_fall_2017_hw3_flags.flags;
 
 import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.res.AssetManager;
 import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
@@ -10,6 +12,7 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
     /* keeps track of the number of continents selected */
@@ -20,6 +23,15 @@ public class MainActivity extends AppCompatActivity {
 
     /* alert dialog for selecting the continents */
     AlertDialog mSelectContinent_Dialog = null;
+
+    /* list of continents in the asset folder */
+    private String[] assetContinents = null;
+
+    /* used to identify the request to send list of continent to game activity */
+    private static final int SEND_CONTINENT_LIST_REQUEST = 1000;
+
+    //  create a local array list to hold the list of continents
+    ArrayList<String> continents = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -52,7 +64,7 @@ public class MainActivity extends AppCompatActivity {
        and creates the arraylist for tracking the selected continents
      */
     private void launchContinentSelector() {
-        Log.d("Ovie", "Create the dialog here");
+        Log.d("DialogMonitor", "Create the dialog here");
         // setup the alert dialog builder here
         AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
         //set the title of the dialog here
@@ -102,6 +114,25 @@ public class MainActivity extends AppCompatActivity {
                     // create an intent to launch the game activity
                     // also figure out a way to save the selected continents
                     // and send this to the next activity
+
+                    //create the intent to move to the next activity
+                    Intent intent = new Intent(MainActivity.this,FlagActivity.class);
+
+                    //save the selected continent in a list and send it to the next activity
+                    // delete --> this is just to show the contents of the selection
+                    System.out.println(mSelectedContinents.toString());
+
+                    //1. get list of continents from the asset folder
+                    getAssetContinents();
+
+                    //2. add the continents to a put extra and send to
+                    //   game activity
+                    intent.putExtra("continents", continents);
+
+                    //3. start the activity for result
+                    startActivityForResult(intent, SEND_CONTINENT_LIST_REQUEST);
+
+
                 }
             }
         });
@@ -127,6 +158,41 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    /*
+        This methods goes through the asset folder and gets all
+        the continents listed there
+     */
+    private void getAssetContinents() {
+        //  use the asset manager to get reference to all the continents
+        //  in the asset folder
+        AssetManager assets = getAssets();
+
+        try {
+            //  since there is no straight forward way to get the
+            //  list of subdirectories in the asset folder
+            //  a general directory was made and all the continents
+            //  were included as subdirectories. That way, assets.list
+            //  would return the list of all subdirectories
+            assetContinents = assets.list("asset_continents");
+
+            //  create a local array list to hold the list of continents
+            continents = new ArrayList<>();
+
+            Log.d("AssetContents", Arrays.toString(assetContinents));
+            for (String index : mSelectedContinents) {
+                String sContinents = assetContinents[Integer.parseInt(index)];
+                continents.add(sContinents);
+            }
+
+            //  test the list of continents to see if it display
+            //  the selected list
+            Log.d("Send", Arrays.toString(continents.toArray()));
+
+        } catch (Exception io) {
+            //  if the asset manager has no content, print no content
+        }
+
+    }
 
 
 }
