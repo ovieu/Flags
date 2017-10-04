2eff0ca28761028eefbd2e2ca3216ce7f85b2a9d
Ovie Umukoro <ovie.u.umukoro@gmail.com>
2017-10-04 17:02:29 -0400

    Receive the selected continents

--- app/src/main/java/cs440_fall_2017_hw3_flags/flags/FlagActivity.java
+++ app/src/main/java/cs440_fall_2017_hw3_flags/flags/FlagActivity.java
@@ -1,13 +1,32 @@
 package cs440_fall_2017_hw3_flags.flags;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class FlagActivity extends AppCompatActivity {
+    /* list of selected country */
+    ArrayList<String> mSelectedContinents = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_flag);
+
+
+        //  first thing is to save the list of continents into
+        //  the intent
+        mSelectedContinents = getIntent().getStringArrayListExtra("continents");
+
+        //this is to test the received list
+        if (mSelectedContinents != null) {
+            Log.d("receiveListTest", Arrays.toString(mSelectedContinents.toArray()));
+        } else {
+            Log.d("receiveListTest", "Nothing was recevied");
+        }
     }
+
 }
